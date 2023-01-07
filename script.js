85ea6d4269a61ddcfbb604e47aefa2d98882dd3b
sv-rubik <tosvr@yandex.ru>
2023-01-07 22:36:47 +0300

    input date added

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 'use strict'
 const newDateDiv = document.querySelector('[data-date="new"]')
-const container = document.querySelector('.container')
+const today = document.querySelector('.today')
+const wrapper = document.querySelector('.wrapper')
+let inputDate
 
 // object to create buttons sections
 const days = {
-  add90: 90,
   add180: 180,
-  add365: 365,
+  add90: 90,
 }
 
-// function to get date = today + days
+// function to get format of date
+function formatDate(inputDate) {
+  let year = inputDate.getFullYear()
+  let month = inputDate.getMonth()
+  let day = inputDate.getDate()
+  switch (month)
+  {
+    case 0: month="Jan"; break
+    case 1: month="Feb"; break
+    case 2: month="Mar"; break
+    case 3: month="Apr"; break
+    case 4: month="May"; break
+    case 5: month="Jun"; break
+    case 6: month="Jul"; break
+    case 7: month="Aug"; break
+    case 8: month="Sept"; break
+    case 9: month="Oct"; break
+    case 10: month="Nov"; break
+    case 11: month="Dec"; break
+  }
+  return String(`${day} ${month} ${year}`)
+  // return date.toDateString().slice(4)
+}
+
+// function to get date = inputDate + days
 function changeDate (numberOfDays){
-  const date = new Date()
-  date.setDate(date.getDate() + numberOfDays)
-  return date.toLocaleDateString()
+  if (inputDate) {
+    const date = new Date(inputDate)
+      date.setDate(inputDate.getDate() + numberOfDays)
+    console.log(inputDate)
+    console.log(date)
+    console.log(inputDate < date)
+      return formatDate(date)
+  } else {
+      const date = new Date()
+      date.setDate(date.getDate() + numberOfDays)
+      return formatDate(date)
+  }
 }
 
 // reflect current date on the page
+today.innerHTML = `<span class="span">Today is </span>${changeDate(0)}`
 newDateDiv.innerHTML = `<span class="span">Today is </span>${changeDate(0)}`
 
 // function to add button-section to page including number if days to add
@@ -34,30 +69,31 @@ function clickListener(obj, index, placeholder) {
   document.querySelector(`[data-days="${Object.keys(obj)[index]}"]`).addEventListener('click', function (e) {
     e.preventDefault()
     placeholder.innerHTML = `<span class="span">${Object.values(obj)[index]} days added </span>${changeDate(Object.values(obj)[index])}`
-    // window.scrollTo(0, -300)
+    if (changeDate(Object.values(obj)[index]) > changeDate(0)) {
+      placeholder.insertAdjacentHTML("beforeend", `<span class="expired"> &mdash; repacking expired!!!</span>`)
+    }
   })
-
 }
 
+// create divs with buttons
 for(let i = 0; i < Object.keys(days).length; i++) {
-  container.insertAdjacentHTML('beforeend', buttonSectionToHtml(days, i))
+  wrapper.insertAdjacentHTML('beforeend', buttonSectionToHtml(days, i))
   clickListener(days, i, newDateDiv)
 }
 
-// input handling
-document.querySelector('.form').addEventListener('submit', function (e) {
+// input numbers handling
+document.querySelector(`[data-type="form-text"]`).addEventListener('submit', function (e) {
   e.preventDefault()
-  let inputValue = +document.querySelector('.form__input').value
+  let inputValue = +document.querySelector(`[data-type="form-input"]`).value
   Number.isInteger(inputValue)
     ? newDateDiv.innerHTML = `<span class="span">${inputValue} days added </span>${changeDate(inputValue)}`
     : alert('Please enter a number of days')
 })
 
-// const datePlus90 = changeDate(days.add90)
-// const datePlus180 = changeDate(days.add180)
-// const datePlus365 = changeDate(days.add365)
-// console.log(datePlus90)
-// console.log(datePlus180)
-// console.log(datePlus365)
+// input date handling
+document.querySelector(`[data-type="form-date"]`).addEventListener('change', function () {
+  inputDate = new Date(document.querySelector(`[data-type="form-date-input"]`).value)
+  newDateDiv.innerHTML = `Last <span class="span">Repacking Date</span> is ${formatDate(inputDate)}`
+})
 
-// TODO - labels, error on wrong input, buttons position center
+// TODO - labels, error on wrong input
