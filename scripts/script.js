fd4dc837f3d43034e7ccc7a17a59cf655f14df71
sv-rubik <tosvr@yandex.ru>
2023-01-23 17:49:24 +0300

    js obj iterator refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,7 @@ function formatDate(inputDate) {
   let month = inputDate.getMonth()
   let day = inputDate.getDate()
   const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]
-  for (let i = 0; i < months.length; i++) {
-    month === i ? month = months[i] : true
-  }
-  return String(`${day} ${month} ${year}`)
+  return String(`${day} ${months[month]} ${year}`)
 }
 // Below can be used instead of formatDate(), however above format is preferred by the App users
 // const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -41,54 +38,55 @@ function changeDate (numberOfDays){
 // function to check if expired and create notification
 function expiryTextCreator(date, placeholder) {
   if (new Date(date) < new Date()) {
-    placeholder.insertAdjacentHTML("beforeend", `<span class="expired"> *repacking has expired!</span>`)
+    placeholder.insertAdjacentHTML("beforeend", `<span class="expired">*repacking has expired!</span>`)
   }
 }
 
-// function to create error notification
+// function to create an error notification if not valid number provided from input form
 function providedValidNumber(inputValue) {
   if(!Number.isInteger(inputValue) || inputValue > 10000 || inputValue <= 0) {
     return alert('Please enter a number of days in range from 1 to 10.000')
   } else {return true}
 }
 
+// function to add button-section to page including number of days to add
+function buttonSectionToHtml(key, value) {
+  return `
+    <div class="button-section">
+      <p class="button-section__text">
+        Add <span class="span">${value} days</span> to selected date
+      </p>
+      <button class="button-section__button" data-days="${key}">Calculate</button>
+    </div>
+  `
+}
+
+// function to add Event listeners on buttons in connection to data-attribute name
+function clickListener(key, value, placeholder) {
+  document.querySelector(`[data-days="${key}"]`).addEventListener('click', e => {
+    e.preventDefault()
+    placeholder.innerHTML = `<div class="new-date__text">
+                                <span class="new-date__span">${value} days added </span>
+                             </div>${changeDate(value)}`
+    expiryTextCreator(changeDate(value), placeholder)
+  })
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
 // reflect current date on the page
 today.innerHTML = `<span class="span">Today is </span>${changeDate(0)}`
 newDateDiv.innerHTML = `<div class="new-date__text">
                           <span class="new-date__span">Today is </span>
                         </div> ${changeDate(0)}`
 
-// function to add button-section to page including number of days to add
-function buttonSectionToHtml(obj, index) {
-  return `
-    <div class="button-section">
-      <p class="button-section__text">
-        Add <span class="span">${Object.values(obj)[index]} days</span> to selected date
-      </p>
-      <button class="button-section__button" data-days="${Object.keys(obj)[index]}">Calculate</button>
-    </div>
-  `
-}
-
-// function to add Event listeners on buttons in connection to data-attribute name
-function clickListener(obj, index, placeholder) {
-  document.querySelector(`[data-days="${Object.keys(obj)[index]}"]`).addEventListener('click', (e) => {
-    e.preventDefault()
-    placeholder.innerHTML = `<div class="new-date__text">
-                                <span class="new-date__span">${Object.values(obj)[index]} days added </span>
-                             </div>${changeDate(Object.values(obj)[index])}`
-    expiryTextCreator(changeDate(Object.values(obj)[index]), placeholder)
-  })
-}
-
 // create divs with buttons
-for(let i = 0; i < Object.keys(days).length; i++) {
-  wrapper.insertAdjacentHTML('beforeend', buttonSectionToHtml(days, i))
-  clickListener(days, i, newDateDiv)
+for(const [key, value] of Object.entries(days)) {
+  wrapper.insertAdjacentHTML('beforeend', buttonSectionToHtml(key, value))
+  clickListener(key, value, newDateDiv)
 }
 
 // number input handling
-document.querySelector(`[data-type="form-text"]`).addEventListener('submit', (e) => {
+document.querySelector(`[data-type="form-text"]`).addEventListener('submit', e => {
   e.preventDefault()
   let inputValue = +document.querySelector(`[data-type="form-input"]`).value
   if (providedValidNumber(inputValue)) {
